11aae1b8ac59b2a3786d4c70e87d3c0598b2959b
Yonas Negese M <yonasnegese250@gmail.com>
2024-07-27 20:03:45 +0300

    Practice request params in express

--- FreeCodeCamp-Node/Express/app.js
+++ FreeCodeCamp-Node/Express/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const {products} = require("./product")
 const app = express();
 
 // // get request
@@ -10,9 +10,49 @@ const app = express();
 // app.all("*",(req,res) => {
 //     res.status(404).send("<h1>Not Found</h1>")
 // })
-const filePath = path.resolve(__dirname,"../Server/index.html")
+// const filePath = path.resolve(__dirname,"../Server/index.html")
 // serving static file
-app.use(express.static("../Server"))
+// app.use(express.static("../Server"))
+
+
+
+// working with api
+app.get("/",(req,res) => {
+    res.send("<h1>Home page</h1><a href='/api/products'>Products</a>")
+})
+
+// product page
+app.get("/api/products",(req,res) => {
+    const newProducts = products.map((product) => {
+        const {id,name,price,image} = product;
+        return  {id,name,price,image};
+    })
+    res.json(newProducts);
+})
+// Get single product
+app.get("/api/products/:productId",(req,res) => {
+    const {productId} = req.params
+    const singleProduct = products.find((product) =>(product.id === productId))
+    if(!singleProduct) {
+        return res.status(404).send("<h1>Product Not Found</h1>")
+    }
+    // console.log(singleProduct)
+    return res.json(singleProduct);
+})
+
+// working with parameters
+app.get("/api/v1/query",(req,res) => {
+    const {search, limit} = req.query;
+    let sortedProducts = [...products];
+
+    if(search) {
+        sortedProducts = sortedProducts.filter((product) => product.name.startsWith(search))
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0,Number(limit));
+    }
+    res.status(200).json(sortedProducts);
+})
 
 // listen to port
 app.listen(3000,() => {
---
+++ FreeCodeCamp-Node/Express/product.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+      "id": "1",
+      "name": "accent chair",
+      "price": 25999,
+      "image": "https://www.course-api.com/images/store/product-1.jpeg",
+      "colors": [
+        "#ff0000",
+        "#00ff00",
+        "#0000ff"
+      ],
+      "company": "marcos",
+      "description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+      "category": "office",
+      "shipping": true
+    },
+    {
+      "id": "2",
+      "name": "elbany sectional",
+      "price": 109999,
+      "image": "https://www.course-api.com/images/store/product-2.jpeg",
+      "colors": [
+        "#000",
+        "#ffb900"
+      ],
+      "company": "liddy",
+      "description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+      "category": "living room"
+    },
+    {
+      "id": "3",
+      "name": "dlbany table",
+      "price": 309999,
+      "image": "https://www.course-api.com/images/store/product-3.jpeg",
+      "colors": [
+        "#ffb900",
+        "#0000ff"
+      ],
+      "company": "liddy",
+      "description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+      "category": "kitchen"
+    },
+    {
+      "id": "4",
+      "name": "bmchair",
+      "price": 12599,
+      "image": "https://www.course-api.com/images/store/product-4.jpeg",
+      "colors": [
+        "#000",
+        "#00ff00",
+        "#0000ff"
+      ],
+      "company": "marcos",
+      "description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+      "category": "bedroom",
+      "shipping": true
+    },
+    {
+      "id": "5",
+      "name": "dining table",
+      "price": 42999,
+      "image": "https://www.course-api.com/images/store/product-5.jpeg",
+      "colors": [
+        "#00ff00",
+        "#0000ff",
+        "#ff0000"
+      ],
+      "company": "ikea",
+      "description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+      "category": "dining",
+      "shipping": true
+    },
+]
+module.exports = {products}

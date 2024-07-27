80d930bba3a5fa7749f11012cc8a07f4ffc5ea1d
Yonas Negese M <yonasnegese250@gmail.com>
2024-07-27 10:28:58 +0300

    Practice file sreams

--- FreeCodeCamp-Node/app.js
+++ FreeCodeCamp-Node/app.js
@@ -45,8 +45,8 @@ const getResult = async() => {
 }
 // getResult()
 // using util for promise
-const fs = require("fs").promises;
-const util = require("util");
+// const fs = require("fs").promises;
+// const util = require("util");
 // const readFilePromise = util.promisify(fs.readFile);
 // const writeFilePromise = util.promisify(fs.writeFile);
 const readAndWrite = async (path) => {
@@ -59,5 +59,58 @@ const readAndWrite = async (path) => {
     }
     
 }
-readAndWrite("./hello.txt")
+// readAndWrite("./hello.txt")
+// event driven programming
+const EventEmitter = require("events")
+const emitter = new EventEmitter();
 
+emitter.addListener("hi",()=>{
+    console.log("Hi, how are you?")
+})
+// emitter.emit("hi")
+// working with streams
+// ********************************
+// const {writeFileSync} = require("fs")
+// for(i = 0;i < 1000;i++) {
+//     writeFileSync("stream.txt",`Hello world ${i}\n`,{flag:'a'})
+// }
+
+
+// const {createReadStream} = require("fs")
+// const stream = createReadStream("./stream.txt",{encoding:"utf8"})
+// stream.on("data",(result)=>{
+//     // console.log(result)
+// })
+
+// *******************
+// send big data with sync
+// const http = require("http")
+// const fs = require("fs")
+// const server = http.createServer((req,res) => {
+//     const file = fs.readFileSync("./stream.txt","utf8");
+//     res.end(file)
+// })
+// server.listen(5000,() => {
+//     console.log("server is listening to 5000")
+// })
+
+// sending big data with stream
+const {createReadStream} = require("fs");
+const http = require("http");
+const server = http.createServer((req,res) => {
+    const file = createReadStream("./stream.txt","utf8");
+    file.on("open",() => {
+        file.pipe(res)
+    })
+    file.on("error",(error)=>{
+        res.end(error)
+        console.log("error:", error)
+    })
+    res.on("finish",()=> {
+        console.log("Sending Finished");
+    })
+})
+
+server.listen(5000,() => {
+    console.log("Server is listening to 5000")
+})

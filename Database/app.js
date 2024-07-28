ca87d52048744fd09244331c71db9dd8305e322e
Yonas Negese M <yonasnegese250@gmail.com>
2024-07-28 22:27:17 +0300

    Practice db in node

--- Database/app.js
+++ Database/app.js
@@ -4,6 +4,9 @@ const mysql2 = require("mysql2")
 // create server
 const app = express();
 
+// body parser
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 // Create connection with database
 const connection = mysql2.createConnection(
     {
@@ -38,7 +41,7 @@ app.get("/submit",(req,res) => {
     // const productDesc = "create table if not exists ProductDescription(desc_id int auto_increment, product_id int not null, product_brief_desc text not null, product_desc text not null, product_image varchar(255) not null, product_link varchar(255) not null, PRIMARY KEY (desc_id), FOREIGN KEY (product_id) REFERENCES Products(product_id))"
 
     // execute
-    // connection.query(productDesc,(err,results,field) => {
+    // connection.query(productDesc,(err,results,fields) => {
     //     if(err) {
     //         console.log(err)
     //     }
@@ -46,22 +49,44 @@ app.get("/submit",(req,res) => {
     // })
 
     // create price table
-    const createProductPrice = "create table if not exists ProductPrice(price_id int auto_increment, product_id int(11) not null, starting_price varchar(255) not null, price_range varchar(255) not null, PRIMARY KEY (price_id), FOREIGN KEY (product_id) REFERENCES Products(product_id))"
-    // execute the query
-    connection.query(createProductPrice,(err,results,field) => {
-        if(err) {
-            console.log("Error",err);
-        }
-        
-    });
+    // const createProductPrice = "create table if not exists ProductPrice(price_id int auto_increment, product_id int(11) not null, starting_price varchar(255) not null, price_range varchar(255) not null, PRIMARY KEY (price_id), FOREIGN KEY (product_id) REFERENCES Products(product_id))"
+    // // execute the query
+    // connection.query(createProductPrice,(err,results,fields) => {
+    //     if(err) {
+    //         console.log("Error",err);
+    //     }
 
-    res.send("Table created!");
+    // });
+
+    res.send(createProductPrice);
 })
 
+// post handler
+app.post("/add",(req,res) => {
+    const iphoneId = req.body.iphoneId
+    const imagePath = req.body.imagePath
+    const url = req.body.iphoneLink
+    const iphoneTitle = req.body.iphoneTitle
+    const startingPrice = req.body.startingPrice
+    const priceRange = req.body.priceRange
+    const briefDesc = req.body.briefDesc
+    const fullDesc = req.body.fullDesc
 
+    // inserting to db
+    const insertProducts = `INSERT INTO Products(product_url,product_name) VALUES('${url}','${iphoneTitle}')`
+
+    // execute the query
+    connection.query(insertProducts,(err,results)=> {
+        if(err) {
+            console.log(err)
+        }
+        console.log(results);
+    })
+    res.send("posted")
+})
 
 // listen to port
-const PORT = 30001 
+const PORT = 3001 
 app.listen(PORT,()=>{
     console.log(`Listening to ${PORT}`)
 })
